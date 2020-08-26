374d70b28c4130cd9fe7eb852b4253a41b9f9fff
Dan <danwventura@gmail.com>
2020-08-26 14:06:00 -0500

    A Letter From Your Niece

--- Tasks_And_Solutions.js
+++ Tasks_And_Solutions.js
@@ -86,3 +86,51 @@ function likes(names) {
   }
 
   /*--------------------------------------------------------------------END OF SOLUTION----------------------------------------------------------------------------------*/
+
+  /*
+    8/26/2020 - A LETTER FROM YOUR NIECE
+
+    Your sweet 4 year old niece has sent you a letter, however she is a little bit of an odd ball, so it needs a bit of decoding. 
+    You see she seems to love dinosars and often throws "trex" and "raptor" in mid sentence when she gets excited, and is convinced all vowels should be replaced by numbers (a=0, e=1, i=2, o=3, u=4).
+
+    Your challenge is to figure out what she is trying to say and fix her grammar- 
+    HINT - All sentences begin with a capital letter, end in a full stop and "I" should be capitalised, 
+    no need for capitals for names, she's only 4 after all :)
+
+    SOLUTION:
+*/
+
+function nonsense(str) {
+  let charArray= str.toLowerCase().replace("trex", "").replace("raptor", "").split('');
+  for (i = 0; i < charArray.length; i++) {
+    switch(charArray[i]){
+        case '0':
+          charArray[i] = 'a'
+          break;
+        case '1':
+          charArray[i] = 'e'
+          break;
+        case '2':
+          charArray[i] = 'i'
+          break;
+        case '3':
+          charArray[i] = 'o'
+          break;
+        case '4':
+          charArray[i] = 'u'
+          break;
+        default:
+          break;
+    }
+  }
+  let cleanedString = charArray.join("");
+  let answerString = cleanedString.charAt(0).toUpperCase() + cleanedString.slice(1);
+  if(answerString.charAt(answerString.length - 1) != "."){
+    answerString += ".";
+  }
+  let regexPattern = /(\si\s)/;
+  let finalString = answerString.replace(regexPattern, " I ");
+  return finalString;
+}
+
+/*--------------------------------------------------------------------END OF SOLUTION----------------------------------------------------------------------------------*/
