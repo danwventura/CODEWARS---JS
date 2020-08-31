161c41cf26f4d04d8895c70d9e143cbd2a879b63
Dan <danwventura@gmail.com>
2020-08-30 20:02:35 -0500

    8.30.2020 - MESSAGE VALIDATOR

--- Tasks_And_Solutions.js
+++ Tasks_And_Solutions.js
@@ -164,3 +164,46 @@ function nonsense(str) {
   }
 
 /*--------------------------------------------------------------------END OF SOLUTION----------------------------------------------------------------------------------*/
+
+/*
+  8/30/2020 - MESSAGE VALIDATOR
+
+In this kata, you have an input string and you should check whether it is a valid message. To decide that, you need to split the string by the numbers, and then compare the numbers with the number of characters in the following substring.
+
+For example "3hey5hello2hi" should be split into 3, hey, 5, hello, 2, hi and the function should return true, because "hey" is 3 characters, "hello" is 5, and "hi" is 2; as the numbers and the character counts match, the result is true.
+
+Notes:
+
+Messages are composed of only letters and digits
+Numbers may have multiple digits: e.g. "4code13hellocodewars" is a valid message
+Every number must match the number of character in the following substring, otherwise the message is invalid: e.g. "hello5" and "2hi2" are invalid
+If the message is an empty string, you should return true
+
+  SOLUTION:
+*/
+
+function isAValidMessage(message){
+  let lastChar = message.slice(-1);
+  let numericRegex = /[0-9]+/;
+  let messageIsValid = true;
+  
+  if(message === ""){
+    return messageIsValid;
+  }
+  else if(!lastChar.match(numericRegex)){
+    let messageContentArray = message.match(/[a-z]+/g);
+    let messageLengthArray = message.match(/[0-9]+/);
+    for(i = 0; i < messageLengthArray.length; i++){
+      let currentMessageLength = parseInt(messageLengthArray[i]);
+      let currentMessage = messageContentArray[i];
+      if(currentMessageLength !== currentMessage.length){
+        messageIsValid = false;
+      }
+      return messageIsValid;
+    }
+  } else {
+    return !messageIsValid;
+  }
+}
+
+/*--------------------------------------------------------------------END OF SOLUTION----------------------------------------------------------------------------------*/
